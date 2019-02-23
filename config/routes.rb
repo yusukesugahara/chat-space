8ed08004bf43d61e9897a6884c6b67bfc4d2f623
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-02-23 11:52:01 +0900

    Created  user_management2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'messages#index'
-  get 'messages' => 'messages#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messeges, only: [:index, :create]
+
+  end
 end
